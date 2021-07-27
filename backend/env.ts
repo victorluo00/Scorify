9e248f6ab3fe4c9a84aa212ef4c1743bff25ac38
eddie246 <72237708+eddie246@users.noreply.github.com>
2021-07-27 12:35:28 -0400

    add routes, controller

--- backend/env.ts
+++ backend/env.ts
@@ -2,4 +2,4 @@ const { config } = require('dotenv');
 
 config();
 
-module.exports = {};
+module.exports = { SPOTIFY_TOKEN, CLIENT_ID, CLIENT_SECRET };
--- backend/server.ts
+++ backend/server.ts
@@ -1,30 +1,60 @@
 import express, { Request, Response, NextFunction } from 'express';
 import path from 'path';
 
-import { playlistRouter } from './routers/index';
+const passport = require('passport');
+const SpotifyStrategy = require('passport-spotify').Strategy;
+const { CLIENT_ID, CLIENT_SECRET } = require('./env.ts');
+
+import { playlistRouter, userRouter } from './routers/index';
 
 const PORT = 3000;
-
 // global middleware
 const app = express();
 app.use(express.json());
-
-// authorization
-// app.get('/auth/spotify', passport.authenticate('spotify'));
-
-// app.get(
-//   '/auth/spotify/callback',
-//   passport.authenticate('spotify', { failureRedirect: '/login' }),
-//   function(req, res) {
-//     // Successful authentication, redirect home.
-//     res.redirect('/');
-//   }
-// );
 
 // root directory, send index.html
 app.get('/', (req, res) => {
   res.status(200).sendFile(path.join(__dirname, '../frontend/index.html'));
 });
+
+//========================================================
+//                     SPOTIFY OAUTH
+//========================================================
+passport.use(
+  new SpotifyStrategy(
+    {
+      clientID: CLIENT_ID,
+      clientSecret: CLIENT_SECRET,
+      callbackURL: 'http://localhost:3000/api/auth/spotify/user',
+    },
+    function (
+      accessToken: String,
+      refreshToken: String,
+      expires_in: Date,
+      profile: Object,
+      done: Function
+    ) {
+      // User.findOrCreate({ spotifyId: profile.id }, function (err, user) {
+      //   return done(err, user);
+      // });
+      console.log(profile);
+      done();
+    }
+  )
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use('/auth/spotify', passport.authenticate('spotify'));
+
+app.get(
+  '/auth/spotify/callback',
+  passport.authenticate('spotify', { failureRedirect: '/login' }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  }
+);
 
 // send js bundle
 // app.get('/dist/bundle.js', (req, res)=>{
@@ -33,6 +63,7 @@ app.get('/', (req, res) => {
 
 // routing
 app.use('/api/playlist', playlistRouter);
+app.use('/api/user', userRouter);
 
 // 404
 app.use((req, res) => {
